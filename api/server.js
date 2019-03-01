9dc08ce22a76993c11bea856a283f1d19d816378
joshua-disney <joshua.a.disney@gmail.com>
2019-03-01 09:39:57 -0700

    Initialize project, install init dependencies, add morgan

--- api/server.js
+++ api/server.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
-const configureRoutes = require('../config/routes.js');
+const configureRoutes = require("../config/routes.js");
 
 const server = express();
 
 server.use(helmet());
 server.use(cors());
+server.use(morgan("dev"));
 server.use(express.json());
 
 configureRoutes(server);
